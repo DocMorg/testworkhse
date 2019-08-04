3b176e00b6000ddabf4e56c3c61c873fe2768fa6
Vlad <vlad_ustimov@mail.ru>
2019-08-04 15:19:30 +0300

    added 2 tests

--- test.py
+++ test.py
@@ -1,9 +1,14 @@
 import unittest
 import project
 import argparse
+import sys
+import io
 
 
 class TestProject(unittest.TestCase):
+    def write(self, arg):
+            pass
+
     def setUp(self):
         self.parser = argparse.ArgumentParser()
         self.parser.add_argument('excel', type=str, action='store',
@@ -16,13 +21,32 @@ class TestProject(unittest.TestCase):
         self.parser.add_argument('-upd', '--update', action='store_true',
                                  help="flag if entered file is to update the existing table")
 
-    def test_empty1(self):  # testing if we have have not entered path to data
+    def test_empty_file(self):  # testing if we have have not entered path to data
         data = ["./test_data/test1.xlsx", "-c"]
-        args = self.parser.parse_args(data)
         with self.assertRaises(SystemExit) as cm:
+            args = self.parser.parse_args(data)
             project.main(args)
-        the_exception = cm.exception
-        self.assertEqual(the_exception.code, 'Table given is empty. Enter any data and try again.')
+        self.assertEqual(cm.exception.code, 'Table given is empty. Enter any data and try again.')
+
+    def test_no_args(self):  # testing with no argument argparse systemexit error == 2
+        # redirect error outputs of argparse lib to nowhere, they are catched, that's ok.
+        sys.stderr = io.StringIO()
+        data = []
+        with self.assertRaises(SystemExit) as cm:
+            args = self.parser.parse_args(data)
+            project.main(args)
+        sys.stderr = sys.__stderr__
+        self.assertEqual(cm.exception.code, 2)
+
+    def test_help(self): # testing argparse --help systemexit == 0 and all ok
+        data = ["-h"]
+        # output suppressed
+        sys.stdout = io.StringIO()
+        with self.assertRaises(SystemExit) as cm:
+            args = self.parser.parse_args(data)
+            project.main(args)
+        sys.stdout = sys.__stdout__
+        self.assertEqual(cm.exception.code, 0)
 
 
 if __name__ == '__main__':
